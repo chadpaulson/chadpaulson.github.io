544404041a336d98a3e152bf3fb84af537170c88
Chad Paulson <chad.paulson@gmail.com>
2020-08-08 13:20:10 -0500

    Increases oscillation of hex background color values.

--- js/behavior.js
+++ js/behavior.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   function trip(){
     $('body').css('background-color','#'+(Math.random()*0xFFFFFF<<0).toString(16));
   }
-  setInterval(trip, 4330);
+  setInterval(trip, 2200);
   trip();
 });
